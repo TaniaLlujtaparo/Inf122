8756d43e311cba7f11d7ff0922b648c0bdd37709
TaniaLlujtaparo <155449861+TaniaLlujtaparo@users.noreply.github.com>
2024-01-15 15:49:34 +0000

    123

---
+++ app-state-event/src/App.js
@@ -0,0 +1,54 @@
+import './App.css';
+import Boton from './components/Boton';
+import Contador from './components/Contador';
+import { useState } from 'react';
+
+function App() {
+  const [nroClicks, setNumClicks] = useState(0);
+
+  const click = () => {
+    setNumClicks(nroClicks + 1);
+    console.log("Click");
+  }
+  
+  const reiniciar = () => {
+    setNumClicks(0);
+    console.log("Reiniciar");
+  }
+  
+
+  return (
+    <div className="App">
+      <div className="contenedor-principal">
+        <Contador nroClicks={nroClicks} />
+        <div className='contendedor-botones'>
+          <Boton texto="-3" esBotonClick={true}
+            funcionClick={click} />
+          <Boton texto="Reiniciar" esBotonClick={false}
+            funcionClick={reiniciar} />
+        </div>
+
+        <div className='contendedor-botones'>
+          <Boton texto="-3" esBotonClick={true}
+            funcionClick={click} />
+          <Boton texto="Reiniciar" esBotonClick={false}
+            funcionClick={reiniciar} />
+        </div>
+        <div className='contendedor-botones'>
+          <Boton texto="-3" esBotonClick={true}
+            funcionClick={click} />
+          <Boton texto="Reiniciar" esBotonClick={false}
+            funcionClick={reiniciar} />
+        </div>
+        <div className='contendedor-botones'>
+          <Boton texto="-3" esBotonClick={true}
+            funcionClick={click} />
+          <Boton texto="Reiniciar" esBotonClick={false}
+            funcionClick={reiniciar} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
